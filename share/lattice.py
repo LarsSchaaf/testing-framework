dc3174719b3ff930f78bbb471216e820106f794b
Lars Schaaf <7270605-LarsSchaaf@users.noreply.gitlab.com>
2021-06-16 14:47:24 +0100

    Adding logging to lattice.py

--- share/lattice.py
+++ share/lattice.py
@@ -5,6 +5,63 @@ from ase.optimize.precon import PreconLBFGS
 import matscipy.elasticity
 from ase.units import GPa
 
+
+import os
+import logging
+import logging.handlers
+import pathlib
+
+DATA_DIR = "/data/lls34/"
+
+LOG_DIR = pathlib.Path(os.path.join(DATA_DIR, "logs/"))
+
+
+def initialize_logger(logger, log_name=None):
+    """
+    Initialize the given `logger` to log to files in the given `log_name` directory.
+    If log_name is None, then no logs are stored.
+    """
+    # Create Main Log directory
+    if os.path.exists(DATA_DIR) and not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR, exist_ok=True)
+
+    # Set up general logger and formatting
+    logger.setLevel(logging.DEBUG)  # listen to everything
+    formatter = logging.Formatter(
+        "%(asctime)s [%(name)-10s] [%(threadName)-12s] %(message)s [in %(pathname)s:%(lineno)d]"
+    )
+
+    # If file_name provided, add file handler for logs
+    if log_name:
+        if not os.path.exists(LOG_DIR / f"{log_name}"):
+            os.makedirs(LOG_DIR / f"{log_name}", exist_ok=True)
+
+        file_handler = logging.handlers.RotatingFileHandler(
+            LOG_DIR / f"{log_name}/{log_name}.log", maxBytes=1000240, backupCount=5
+        )
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
+
+    # Add console handler for logs:
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_formatter = logging.Formatter("[%(name)-10s] %(message)s")
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+
+# logging.basicConfig(
+#     filename=__file__[-3] + ".txt",
+#     filemode="a",
+#     format="%(asctime)s %(msecs)d- %(process)d-%(levelname)s - %(message)s",
+#     datefmt="%d-%b-%y %H:%M:%S %p",
+#     level=logging.DEBUG,
+# )
+
+log = logging.getLogger(__name__)
+initialize_logger(log, log_name=__name__)
+
 # Calculate B for Hexagonal, Tetragonal & Trigonal
 # DOI: 10.1103/PhysRevB.77.104118 Eq. (29)
 def HTT_B(c11, c33, c12, c13):
@@ -28,7 +85,7 @@ def VRH_B(c11, c33, c12, c13, c44, c66):
 def calc_E_vs_V(
     bulk, dV=0.025, n_steps=(-10, 10), tol=1e-2, method="lbfgs"
 ):  # hack tol to deal with Te C2/m
-    '''
+    """
     Run the E vs V analysis
 
     Changes the cell size isometrically to scan for different volumes and scales
@@ -45,30 +102,44 @@ def calc_E_vs_V(
 
     Returns:
         list: results of E vs V investigation (Volume/num_at, Energy/num_at, Stress)
-    '''
+    """
     import model
+
+    log.debug(f"Starting calc_E_vs_V for {model.name}")
 
     V0 = bulk.get_volume()
     dV *= V0
     E_vs_V = []
 
     scaled_bulk = bulk.copy()
+
+    # Firstly Reduce Volume
+    log.debug("Runs for decreasing Volume: ")
+
     for i in range(0, n_steps[0] - 1, -1):
+        log.info(f"    Increased volume {i}/{n_steps[0]}")
         V_cur = scaled_bulk.get_volume()
+
+        # Increase volume with scaled atoms
         scaled_bulk.set_cell(
             scaled_bulk.get_cell() * ((V0 + i * dV) / V_cur) ** (1.0 / 3.0),
             scale_atoms=True,
         )
-        ase.io.write(sys.stdout, scaled_bulk, format="extxyz")
+        # ase.io.write(sys.stdout, scaled_bulk, format="extxyz")
         print("trying to relax i", i)
         try:
             if hasattr(model, "fix_cell_dependence"):
                 model.fix_cell_dependence(scaled_bulk)
+
+            # Writing unlrelaxed
+            to_fname = run_root + "-E_vs_V_%03d-unrelaxed.xyz" % i
+            log.debug(f"    - writing unrelaxed {to_fname}")
             ase.io.write(
-                run_root + "-E_vs_V_%03d-unrelaxed.xyz" % i,
-                scaled_bulk,
-                format="extxyz",
+                to_fname, scaled_bulk, format="extxyz",
             )
+
+            # Relax config
+            log.debug(f"    - About to relax using relax_config() call")
             scaled_bulk = relax_config(
                 scaled_bulk,
                 relax_pos=True,
@@ -85,10 +156,15 @@ def calc_E_vs_V(
                 save_config=True,
             )
         except Exception as e:
-            print("WARNING: failed config in calc_E_vs_V", str(e))
+            msg = "WARNING: failed config in calc_E_vs_V", str(e)
+            log.error(msg)
+            print(msg)
             sys.exit(1)  #### NB
             break
-        ase.io.write(sys.stdout, scaled_bulk, format="extxyz")
+
+        # ase.io.write(sys.stdout, scaled_bulk, format="extxyz")
+
+        log.debug(f"    - Appending E_vs_V dictionary")
         E_vs_V.insert(
             0,
             (
@@ -98,8 +174,12 @@ def calc_E_vs_V(
             ),
         )
 
+    # Increase Volume
+
+    log.debug("Runs for increasing Volume: ")
     scaled_bulk = bulk.copy()
     for i in range(1, n_steps[1] + 1):
+        log.debug(f"    Increased volume {i}/{n_steps[1]}")
         V_cur = scaled_bulk.get_volume()
         scaled_bulk.set_cell(
             scaled_bulk.get_cell() * ((V0 + i * dV) / V_cur) ** (1.0 / 3.0),
